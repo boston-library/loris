fb49a5b781dfbb43cfe0d2c40dea9c289eb3b122
Steven Anderson <sanderson@bpl.org>
2014-10-31 17:20:00 -0400

    Some quick hacking for a local file find install

--- loris/resolver.py
+++ loris/resolver.py
@@ -11,6 +11,7 @@ from shutil import copy
 from os import makedirs
 from os.path import dirname
 from loris_exception import ResolverException
+import fnmatch
 
 logger = getLogger(__name__)
 
@@ -84,6 +85,67 @@ class SimpleFSResolver(_AbstractResolver):
         logger.debug('src format %s' % (format,))
 
         return (fp, format)
+
+class SimpleBPLResolver(_AbstractResolver):
+
+    def __init__(self, config):
+        super(SimpleBPLResolver, self).__init__(config)
+        self.cache_root = self.config['src_img_root']
+
+    def is_resolvable(self, ident):
+        ident = unquote(ident)
+        ident = ident.replace(':', '%3A')
+        target_string = "info%3Afedora%2F" + ident + "%2FaccessMaster%2FaccessMaster.0"
+        #fp = find(target_string, '/home/fedora/fedora36/data/datastreamStore')
+        fp = SimpleBPLResolver._find(target_string, self.cache_root)
+
+        if len(fp) == 0:
+            return False
+        else:
+            return True
+        #fp = join(self.cache_root, ident)
+
+        #return exists(fp)
+
+    @staticmethod
+    def _format_from_ident(ident):
+        if ident.find('.') == -1:
+            return 'jp2'
+        else:
+            return ident.split('.')[-1]
+
+    def resolve(self, ident):
+        # For this dumb version a constant path is prepended to the identfier
+        # supplied to get the path It assumes this 'identifier' ends with a file
+        # extension from which the format is then derived.
+        ident = unquote(ident)
+
+        ident = ident.replace(':', '%3A')
+        target_string = "info%3Afedora%2F" + ident + "%2FaccessMaster%2FaccessMaster.0"
+        #fp = find(target_string, '/home/fedora/fedora36/data/datastreamStore')
+        fp = SimpleBPLResolver._find(target_string, self.cache_root)[0]
+        #fp = join(self.cache_root, ident)
+        logger.debug('src image: %s' % (fp,))
+
+        if not exists(fp):
+            public_message = 'Source image not found for identifier: %s.' % (ident,)
+            log_message = 'Source image not found at %s for identifier: %s.' % (fp,ident)
+            logger.warn(log_message)
+            raise ResolverException(404, public_message)
+
+        format = SimpleBPLResolver._format_from_ident(ident)
+        logger.debug('src format %s' % (format,))
+
+        return (fp, format)
+
+    #From: http://stackoverflow.com/questions/1724693/find-a-file-in-python
+    def _find(pattern, path):
+        result = []
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if fnmatch.fnmatch(name, pattern):
+                    result.append(os.path.join(root, name))
+        return result
 
 class SourceImageCachingResolver(_AbstractResolver):
     '''
